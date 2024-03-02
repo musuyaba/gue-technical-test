436aa5c53f3052d665ce5dc1c58f6ff7172fa2bc
msyb <musuyaba@gmail.com>
2024-03-02 14:09:31 +0700

    feat: Add consumer package.json and index.js files

---
+++ consumer/index.js
@@ -0,0 +1,24 @@
+const { Kafka } = require('kafkajs')
+
+const consume = async function consumeTopic() {
+    const kafka = new Kafka({
+        clientId: 'consumer-node-1',
+        brokers: ['localhost:9092'],
+    })
+
+    const consumer = kafka.consumer({ groupId: 'network-1' })
+
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log({
+                key: message.key.toString(),
+                value: message.value.toString(),
+            })
+        },
+    })
+}
+
+consume().catch(console.error)
