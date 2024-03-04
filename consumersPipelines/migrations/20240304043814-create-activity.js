5bb264c8a0bc790feaa711ccebf77619ecc19586
msyb <musuyaba@gmail.com>
2024-03-04 12:41:36 +0700

    feat: Add activity and count columns to database tables

---
+++ consumersPipelines/migrations/20240304043814-create-activity.js
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Activities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      old: {
+        type: Sequelize.JSONB
+      },
+      new: {
+        type: Sequelize.JSONB
+      },
+      submitlogs_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'SubmitLogs',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Activities');
+  }
+};
